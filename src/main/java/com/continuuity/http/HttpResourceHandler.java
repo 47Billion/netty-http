03b55ac186384a4c7d0395cd0161ed56db07b124
Gowri Shankar Panneer Selvam <shankar@Gowris-MacBook-Pro.local>
2014-04-22 12:08:51 -0700

    checkstyle fix

--- src/main/java/com/continuuity/http/HttpResourceHandler.java
+++ src/main/java/com/continuuity/http/HttpResourceHandler.java
@@ -215,7 +215,8 @@ public final class HttpResourceHandler implements HttpHandler {
         }
       } catch (Throwable t) {
         LOG.error("Exception thrown during rewriting of uri {}", request.getUri(), t);
-        responder.sendError(HttpResponseStatus.INTERNAL_SERVER_ERROR, String.format("Caught exception processing request. Reason: %s", t.getMessage()));
+        responder.sendError(HttpResponseStatus.INTERNAL_SERVER_ERROR, String.format
+          ("Caught exception processing request. Reason: %s", t.getMessage()));
       }
     }
 
